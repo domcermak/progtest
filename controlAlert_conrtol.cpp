2a70bd48632aa2ea1588bd57448f3b49b09ef799
Dominik Čermák <domcermak64@gmail.com>
2017-04-14 01:46:54 +0200

    Add files via upload

--- controlAlert_conrtol.cpp
+++ controlAlert_conrtol.cpp
@@ -19,107 +19,102 @@ using namespace std;
 
 class CDate
 {
-  public:
+public:
     //---------------------------------------------------------------------------------------------
-                             CDate                         ( int               y,
-                                                             int               m,
-                                                             int               d )
-                             : m_Year ( y ),
-                               m_Month ( m ),
-                               m_Day ( d )
-    {
-    }
+    CDate ( int y, int m, int d )
+        : m_Year ( y ), m_Month ( m ), m_Day ( d ) {}
     //---------------------------------------------------------------------------------------------
     int                      Compare                       ( const CDate     & x ) const
     {
-      if ( m_Year != x . m_Year )
-        return m_Year - x . m_Year;
-      if ( m_Month != x . m_Month )
-        return m_Month - x . m_Month;
-      return m_Day - x . m_Day;
+        if ( m_Year != x . m_Year )
+            return m_Year - x . m_Year;
+        if ( m_Month != x . m_Month )
+            return m_Month - x . m_Month;
+        return m_Day - x . m_Day;
     }
     //---------------------------------------------------------------------------------------------
     int                      Year                          ( void ) const 
     {
-      return m_Year;
+        return m_Year;
     }
     //---------------------------------------------------------------------------------------------
     int                      Month                         ( void ) const 
     {
-      return m_Month;
+        return m_Month;
     }
     //---------------------------------------------------------------------------------------------
     int                      Day                           ( void ) const 
     {
-      return m_Day;
+        return m_Day;
     }
     //---------------------------------------------------------------------------------------------
-    friend ostream         & operator <<                   ( ostream         & os,
-                                                             const CDate     & x )
+    friend ostream & operator << ( ostream & os,
+        const CDate & x )
     {
-      char oldFill = os . fill ();
-      return os << setfill ( '0' ) << setw ( 4 ) << x . m_Year << "-" 
-                                   << setw ( 2 ) << (int) x . m_Month << "-" 
-                                   << setw ( 2 ) << (int) x . m_Day 
-                << setfill ( oldFill );
+        char oldFill = os . fill ();
+        return os << setfill ( '0' ) << setw ( 4 ) << x . m_Year << "-" 
+                                     << setw ( 2 ) << (int) x . m_Month << "-" 
+                                     << setw ( 2 ) << (int) x . m_Day 
+                                     << setfill ( oldFill );
     }
     //---------------------------------------------------------------------------------------------
-  private:
-    int16_t                  m_Year;
-    int8_t                   m_Month;
-    int8_t                   m_Day;
+private:
+    int16_t m_Year;
+    int8_t m_Month;
+    int8_t m_Day;
 };
 #endif /* __PROGTEST__ */
 
 class CInvoice
 {
-  public:
-                             CInvoice                      ( const CDate     & date,
-                                                             const string    & seller,
-                                                             const string    & buyer,
-                                                             unsigned int      amount,
-                                                             double            VAT );
-    CDate                    Date                          ( void ) const;
-    string                   Buyer                         ( void ) const;
-    string                   Seller                        ( void ) const;
-    int                      Amount                        ( void ) const;
-    double                   VAT                           ( void ) const;
-  private:
+public:
+    CInvoice ( const CDate     & date,
+               const string    & seller,
+               const string    & buyer,
+               unsigned int      amount,
+               double            VAT );
+    CDate Date ( void ) const;
+    string Buyer ( void ) const;
+    string Seller ( void ) const;
+    int    Amount ( void ) const;
+    double VAT ( void ) const;
+private:
     // todo
 };
 
 class CSortOpt
 {
-  public:
-    static const int         BY_DATE                       = 0;
-    static const int         BY_BUYER                      = 1;
-    static const int         BY_SELLER                     = 2;
-    static const int         BY_AMOUNT                     = 3;
-    static const int         BY_VAT                        = 4;
-                             CSortOpt                      ( void );
-    CSortOpt               & AddKey                        ( int               key,
-                                                             bool              ascending = true );
-  private:
-    // todo
+public:
+    static const int BY_DATE   = 0;
+    static const int BY_BUYER  = 1;
+    static const int BY_SELLER = 2;
+    static const int BY_AMOUNT = 3;
+    static const int BY_VAT    = 4;
+    CSortOpt ( void );
+    CSortOpt & AddKey ( int key,
+                        bool ascending = true );
+private:
+  // todo
 };
 
 class CVATRegister
 {
-  public:
-                             CVATRegister                  ( void );
-    bool                     RegisterCompany               ( const string    & name );
-    bool                     AddIssued                     ( const CInvoice  & x );
-    bool                     AddAccepted                   ( const CInvoice  & x );
-    bool                     DelIssued                     ( const CInvoice  & x );
-    bool                     DelAccepted                   ( const CInvoice  & x );
-    list<CInvoice>           Unmatched                     ( const string    & company,
-                                                             const CSortOpt  & sortBy ) const;
-  private:
-    // todo
+public:
+    CVATRegister ( void );
+    bool RegisterCompany ( const string    & name );
+    bool AddIssued ( const CInvoice  & x );
+    bool AddAccepted ( const CInvoice  & x );
+    bool DelIssued ( const CInvoice  & x );
+    bool DelAccepted ( const CInvoice  & x );
+    list<CInvoice> Unmatched ( const string    & company,
+                               const CSortOpt  & sortBy ) const;
+private:
+  // todo
 };
 
 #ifndef __PROGTEST__
-bool equalLists ( const list<CInvoice> & a, const list<CInvoice> & b )
+bool equalLists ( const list<CInvoice> & a, 
+                  const list<CInvoice> & b )
 {
   // todo
 }
