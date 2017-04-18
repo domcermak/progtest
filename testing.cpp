dcc2e65e538b781a52b9f51736741ce60ff8fbd8
Dominik Čermák <domcermak64@gmail.com>
2017-04-18 14:37:43 +0200

    Add files via upload

--- testing.cpp
+++ testing.cpp
@@ -19,30 +19,21 @@ using namespace std;
 class CVATRegister
 {
 public:
-    CVATRegister ( void )
+    CVATRegister ( void ) : m_reg_size ( 100 ), m_reg_items_cnt ( 0 )
     {
-        m_reg_size = 100;
-        m_reg_items_cnt = 0;
         m_reg_array = new T_Registry [ m_reg_size ];
     }
 
     bool RegisterCompany ( const string & name )
     {
-        // search if !inserted
-        for ( unsigned int i = 0; i < m_reg_items_cnt; i++ )
-        {
-            if ( check_if_inserted ( name ) )
-                return false;
-        }
+        if ( check_if_inserted ( name.c_str () ) )
+            return false;
         if ( m_reg_items_cnt >= m_reg_size )
             array_realloc ( m_reg_array, m_reg_size );
-
-
-
-
-
-
-
+        
+        strcpy ( this->m_reg_array [ m_reg_items_cnt ].m_original, name.c_str () );
+        m_reg_items_cnt++;
+        qsort_name ( m_reg_array, m_reg_items_cnt );
         return true;
     }
     /*
@@ -61,9 +52,10 @@ private:
 
     struct T_Registry
     {
-        char * m_reg_name;
+        char * m_original; // original input
+        char * reduced; // prepared to search
 
-        T_Issued * m_iss_array;
+        T_Issued * m_iss_array; // issued
         unsigned int m_iss_items_cnt;
         unsigned int m_iss_size;
     };
@@ -81,7 +73,7 @@ private:
         {
             tmp_array [ i ] = arr [ i ];
         }
-        arr_size = tmp_array;
+        arr_size = tmp_size;
         delete arr;
         arr = tmp_array;
     }
@@ -94,76 +86,116 @@ private:
         {
             tmp_array [ i ] = arr [ i ];
         }
-        arr_size = tmp_array;
+        arr_size = tmp_size;
         delete arr;
         arr = tmp_array;
     }
     // binary search / sort -----------------------------------------------------
-    int qsearch_name ( string name_to_find, 
+    // returns -1 if !found
+    int qsearch_name ( char * name_to_find, 
                        unsigned int from_,
-                       unsigned int to_ /* item_cnt - 1 */ )
+                       unsigned int to_ ) // to_ = items_cnt - 1
     {
         int ret_val;
         unsigned int search_pos = ( ( to_ - from_ ) + 1 ) / 2; // size / 2
 
-        ret_val = strcmp ( name_to_find, this->m_reg_array [ search_pos ].m_reg_name );
+        ret_val = strcmp ( name_to_find, this->m_reg_array [ search_pos ].reduced );
         if ( !ret_val )
             return search_pos;
         if ( ( ret_val > 0 || ret_val < 0 ) && to_ == from_ )
             return -1;
-        ( ret_val > 0 )
-            ? return qsearch_name ( name_to_find, search_pos + 1, to_ )
-            : return qsearch_name ( name_to_find, from_, search_pos - 1 );
-    } // returns -1 if !found
+        
+         return ( ret_val > 0 )
+            ? qsearch_name ( name_to_find, search_pos + 1, to_ )
+            : qsearch_name ( name_to_find, from_, search_pos - 1 );
+    } 
 
-    void qsort_name ( T_Registry *& arr, unsigned int size );
+    void qsort_name ( T_Registry *& arr, unsigned int size )
+    {
+        unsigned int pivot = size - 2;
+
+        // todo
+    }
 
     //---------------------------------------------------------------------------
     bool is_upper_alpha ( char c )
     {
-        ( c >= 'A' && c <= 'Z' ) ? return true : return false;
+        return ( c >= 'A' && c <= 'Z' ) ? true : false;
     }
 
     bool is_space ( char c )
     {
-        ( c == " " ) ? return true : return false;
+        return ( c == ' ' ) ? true : false;
     }
     
-    void to_lower_case ( const string str )
+    void to_lower_case (  char *& str )
     {
         for ( unsigned int i = 0; i < strlen ( str ); i++ )
             if ( is_upper_alpha ( str [ i ] ) )
                 str [ i ] += 'a' - 'A';
     }
 
-    void spaces_reduction ( const string str, unsigned int size )
+    void spaces_reduction ( char *& str, unsigned int & size )
     {
-        bool other_noticed = false;
-        
-        int * spaces = new int [ size - 1 ],
-            last_set = -1;
+        unsigned int i = 0;
+        char * tmp;
 
-        for ( unsigned int i = 0; i < size - 1; i++ )
+        while ( is_space ( str [ i ] ) )
+            i++;
+
+        // spaces before
+        if ( !i )
         {
-            if ( is_space ( str [ i ] )
-                && is_space ( str [ i + 1 ] ) )
+            size -= i;
+            tmp = new char [ size ];
+            for ( unsigned int k = i; k < size; k++ )
+                tmp [ k - i ] = str [ k ];
+            delete str;
+            str = tmp;
+        }
+
+        // spaces between
+        for ( unsigned int k = 0; k < size; k++ )
+        {
+            if ( is_space ( str [ k ] ) 
+                && is_space ( str [ k + 1 ] ) )
             {
-                spaces [ last_set++ ] = i;
+                char * tmp = new char [ size-- ];
+                for ( unsigned int f = 0; f < size; f++ )
+                    tmp [ f ] = ( f < k ) ? str [ f ] : str [ f + 1 ];
+                delete str;
+                str = tmp;
             }
         }
-        // todo
+
+        // spaces after
+        if ( is_space ( str [ size - 1 ] ) )
+        {
+            char * tmp = new char [ size-- ];
+            for ( unsigned int k = 0; k < size; k++ )
+                tmp [ k ] = str [ k ];
+            delete str;
+            str = tmp;
+        }
     }
 
-    bool check_if_inserted ( const string str )
+    bool check_if_inserted ( const char * str )
         // case sensitivity : disabled
         // more spaces : reduce
     {
+        char * non_const_str;
         unsigned int size = strlen ( str );
 
-        to_lower_case ( str );
-        spaces_reduction ( str, size );
-        if ( -1 != qsearch_name ( str, 0, this->m_reg_items_cnt - 1 ) )
+        non_const_str = new char [ size ];
+        strcpy ( non_const_str, str );
+        to_lower_case ( non_const_str );
+        spaces_reduction ( non_const_str, size );
+        
+        if ( -1 != qsearch_name ( non_const_str, 0, this->m_reg_items_cnt - 1 ) )
             return true;
+
+        strcpy ( this->m_reg_array [ m_reg_items_cnt ].reduced, non_const_str );
+        return false;
     }
 };
 
