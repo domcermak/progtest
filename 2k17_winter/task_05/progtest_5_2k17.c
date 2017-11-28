a5dea3f4a56c85561db109bed5a685b6eafd99a1
Dominik <domcermak64@icloud.com>
2017-11-28 16:32:21 +0100

    Improve tester and modify 2k17_winter/task_05/progtest_5_2k17.c

---
+++ 2k17_winter/task_05/progtest_5_2k17.c
@@ -0,0 +1,25 @@
+//
+//  Created by Dominik Čermák as a part of http://www.github.com/domcermak/progtest
+//  Copyright © 2017 Dominik Čermák. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <assert.h>
+
+
+
+
+
+
+
+
+
+//  run
+int main (void) {
+    
+    
+    return 0;
+}
