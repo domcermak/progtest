19cbe7e81acb9b3a3cf5ac1ae03eee40306174f7
Dominik <domcermak64@icloud.com>
2017-11-29 23:41:44 +0100

    Add calculation of lowest common multiple used as result

--- 2k17_winter/task_05/progtest_5_2k17.c
+++ 2k17_winter/task_05/progtest_5_2k17.c
@@ -9,24 +9,42 @@
 #include <stdbool.h>
 #include <assert.h>
 
+//
+//  Struct of stripe
+//  @param array
+//  @param length
+//
 typedef struct {
 	int * array;
-	size_t lenght;
+	size_t length;
 } stripe;
 
+//
+//  Struct of way values
+//  @param from
+//  @param to
+//  @param eof
+//
 typedef struct {
 	int from;
 	int to;
+	bool eof;
 } way;
 
+//
+//  Print error message and exit program
+//
 static void wrongInput (void) {
 	printf("Nespravny vstup.\n");
 	exit(EXIT_FAILURE);	
 }
 
-static const stripe loadStripes (void) {
+//
+//  Load stripes values in preset form
+//  @return List of stripes
+//
+static stripe loadStripes (void) {
 	stripe myStripe = {NULL, 0};
-	int ref;
 	int * result = (int*) malloc (sizeof(int));
 	size_t mySize = 1;
 
@@ -37,10 +55,10 @@ static const stripe loadStripes (void) {
 		int value = 0;
 		char str[2], c;
 
-		ref = scanf("%1s %d", c, &value);
+		ref = scanf("%1s %d", str, &value);
 		c = str[0];
 
-		if (!i && c != "{") wrongInput();
+		if (!i && c != '{') wrongInput();
 		if (i) {
 			if (c == '}') {
 				myStripe.array = result;
@@ -49,34 +67,86 @@ static const stripe loadStripes (void) {
 				return myStripe;
 			}
 			if (ref != 2 || c != ',' || value < 1)
-				wrongInput()
+                wrongInput();
 		}
 		
 		if (mySize <= i) {
 			mySize *= 2;
-			result = (int*) realloc (mySize * sizeof(int));	
+			result = (int*) realloc (result, mySize * sizeof(int));
 		}
 
 		*(result + i) = value;
 	}
 }
 
+//
+//  Load way values
+//  @param s Stripe list with filled values
+//
 static way loadWay (const stripe * s) {
-	way result = {0, 0};
+	way result = {0, 0, false};
 	int ref, from, to;
 	
 	ref = scanf("%d %d", &from, &to);
-	if (ref != 2 || from < 0 || from >= to || s.length < to)
+	if (ref == EOF) {
+		result.eof = true;
+		return result;
+	}
+	if (ref != 2 || from < 0 || from >= to || s->length < (size_t)to)
 		wrongInput();
 
 	return result;
 }
 
+//
+//  Greatest common divisor
+//  @param a Number a
+//  @param b Number b
+//
+static int gcd (const int a, const int b) {
+    return !b ? a : gcd(b, a % b);
+}
+
+//
+//  Lowest common multiple
+//  @param a Number a
+//  @param b Number b
+//
+static int lcm (const int a, const int b) {
+    return (a * b) / gcd(a, b);
+}
+
+static int carCountCalc(const stripe * myStripe, const way * myWay) {
+    int result = myStripe->array[myWay->from];
+    
+    for (int i = myWay->from + 1; i < myWay->to + 1; i++)
+        result = lcm(result, myStripe->array[i]);
+    
+    return result;
+}
+
+//
+//  Deallocate allocated list
+//  @param myList Stripe list with allocated array
+//
+static void removeStripes (stripe * myList) {
+    free(myList->array);
+}
+
 //  run
 int main (void) {
-    myStripe stripeList = loadStripes();
+    stripe stripeList = loadStripes();
+	
+	printf("Trasy:\n");
+	while (true) {
+		way myWay = loadWay(&stripeList);
+		int cnt;
 
-	free(stripesList);
+		if (myWay.eof) break;
+		cnt = carCountCalc(&stripeList, &myWay);
+		printf("Vozidel: %d\n", cnt);
+	}
+    removeStripes(&stripeList);
     
     return 0;
 }
