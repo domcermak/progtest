144dd4f62d9de545da09c0ef08941bdecbf2dfb6
Dominik <domcermak64@icloud.com>
2017-12-11 19:37:37 +0100

    Implement loading system, parsing, and sorting in task 06

--- 2k17_winter/task_06/progtest_6_2k17.c
+++ 2k17_winter/task_06/progtest_6_2k17.c
@@ -10,20 +10,8 @@
 typedef struct {
     char * word;
     size_t length;
-    int indexInOrigin;
+    int * indexInOrigin;
 } string;
-
-//  Node in binary tree
-//  @param left                 Pointer to node on the left side
-//  @param right                Pointer to node on the right side
-//  @param word                 String in node
-//  @param originPositionIndex  Index where word start in original sentence
-struct wordNode {
-    struct wordNode * left;
-    struct wordNode * right;
-    string * word;
-    int originPositionIndex;
-};
 
 //  Print error message and exit program with value 1
 void wrongInput (void) {
@@ -31,41 +19,46 @@ void wrongInput (void) {
     exit(EXIT_FAILURE);
 }
 
-//  Verify if parameter is defined whitespace
-//  @param c    Compared character
-//  @return     True if parameter is space or newline
-bool isWhite (const char c) {
-    return c == ' ' || c == '\n';
-}
-
 //  Verify if parameter is alpha
 //  @param c    Compared character
 //  @return     True if parameter is low or high alpha
-bool isAlpha (const char c) {
+static bool isAlpha (const char c) {
     return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
 }
 
 //  Transform high alpha to low equivalent
 //  @param c    Compared character
 //  @return     Lower aplha or original parameter
-char toLower (const char c) {
-    if (isAlpha(c)) {
-        return c >= 'a' && c <= 'z' ? c : (char)(c + 'a' - 'A');
-    }
-    
-    return c;
+static char toLower (const char c) {
+    return c >= 'A' && c <= 'Z' ? (char)(c + 'a' - 'A') : c;
 }
 
 //  Transform newline to space character
 //  @param c    Compared character
 //  @return     Space or original character
-char toSpace (const char c) {
+static char toSpace (const char c) {
     return c == '\n' ? ' ' : c;
 }
 
+#ifndef __PROGTEST__
+static void printSentence(string ** wordList, const size_t length) {
+    for (size_t i = 0; i < length; i++) {
+        string * word = *(wordList + i);
+        
+        for (size_t k = 0; k < word->length; k++) {
+            printf("%c", *(word->word + k));
+        }
+        
+        printf(" -> %d\n", *word->indexInOrigin);
+    }
+    
+    exit(EXIT_SUCCESS);
+}
+#endif // __PROGTEST__
+
 //  Load sentence from standard input
 //  @return     Allocated sentence wrapped in string struct
-string * loadSentence (void) {
+static string * loadSentence (void) {
     string * sentence = (string*) malloc(sizeof(string));
     char * word = NULL;
     size_t mySize = 0, length = 0;
@@ -122,6 +115,7 @@ string * loadSentence (void) {
     
     sentence->word = word;
     sentence->length = length;
+    sentence->indexInOrigin = NULL;
     if (!sentence->length) wrongInput();
     
     return sentence;
@@ -129,27 +123,30 @@ string * loadSentence (void) {
 
 //  Select only words from sentence
 //  @param sentence Sentence to be parsed
+//  @param length   Count of strings in separated array
 //  @return         Allocated square array with separated words
-string ** separate (const string * sentence, size_t * length) {
+static string ** separate (const string * sentence, size_t * length) {
     string ** result = (string**) malloc(sizeof(string*));
     string * sepString = (string*) malloc(sizeof(string));
     size_t wordSize = 1, resSize = 1;
     int lastSpaceIndex = -1;
     
     *length = 0;
-    sepString->indexInOrigin = 0;
+    sepString->indexInOrigin = (int*) malloc(sizeof(int));
+    *sepString->indexInOrigin = 0;
     sepString->length = 0;
     sepString->word = (char*) malloc(sizeof(char));
     
+    
     for (int i = 0; i < sentence->length; i++) {
         char c = sentence->word[i];
-        
+
         if (isAlpha(c)) {
             if (lastSpaceIndex >= wordSize) {
                 wordSize *= 2;
                 sepString->word = (char*)realloc(sepString->word, wordSize * sizeof(char));
             }
-            
+
             sepString->length++;
             *(sepString->word + i - lastSpaceIndex - 1) = c;
         }
@@ -158,18 +155,21 @@ string ** separate (const string * sentence, size_t * length) {
                 resSize *= 2;
                 result = (string**) realloc(result, resSize * sizeof(string*));
             }
-            
+
             *(result + *length) = sepString;
             ++*length;
+
             sepString = (string*) malloc(sizeof(string));
-            sepString->indexInOrigin = i + 1;
+            sepString->indexInOrigin = (int*) malloc(sizeof(int));
+            *sepString->indexInOrigin = i + 1;
             sepString->length = 0;
             sepString->word = (char*) malloc(sizeof(char));
+
             wordSize = 1;
             lastSpaceIndex = i;
         }
     }
-    
+
     if (*length >= resSize) {
         resSize++;
         result = (string**) realloc(result, resSize * sizeof(string*));
@@ -180,39 +180,46 @@ string ** separate (const string * sentence, size_t * length) {
     return result;
 }
 
-void printSentence(string ** sentence, const size_t length) {
-    for (size_t i = 0; i < length; i++) {
-        for (size_t k = 0; k < (*(sentence + i))->length; k++) {
-            printf("%c", *((*(sentence + i))->word + k));
-        }
-        printf("\n");
-    }
+//  Compare function created for quick sort function to sort items type of string**
+//  @param a Compared item 1
+//  @param b Compared item 2
+//  @return 0 when equal, >0 if a > b, <0 if a < b
+static int compare (const void * a, const void * b) {
+    string * ia = *(string**)a, *ib = *(string**)b;
+    size_t len = ia->length;
     
-    exit(EXIT_SUCCESS);
+    if (ia->length != ib->length)
+        len = ia->length > ib->length ? ib->length : ia->length;
+
+    for (size_t i = 0; i < len; i++)
+        if (*(ia->word + i) != *(ib->word + i))
+            return *(ia->word + i) > *(ib->word + i) ? 1 : -1;
+    
+    if (ia->length != ib->length)
+        return (int)(ia->length > ib->length ? ia->length : ib->length);
+    
+    return 0;
 }
 
 //  Create binary tree from origin sentence
 //  @param origin   Original sentence in one array
 //  @return         Pointer to root of binary tree created from origin sentence
-struct wordNode * parseSentence (const string * sentence) {
+static string * parseSentence (const string * sentence) {
     size_t sepLength;
-    struct wordNode * root = NULL;
+    string * list = NULL;
     string ** separateWords = separate(sentence, &sepLength);
     
-    printSentence(separateWords, sepLength);
+    qsort(separateWords, sepLength, sizeof(string*), compare);
     
-//    sort
-//    order to binary tree
+//    unique
     
-    return root;
+    return list;
 }
 
 //	run
 int main (void) {
-    bool eof;
     string * sentenceOrigin = loadSentence();
-    
-    struct wordNode * root = parseSentence(sentenceOrigin);
+    string * list = parseSentence(sentenceOrigin);
     
 //    for (int i = 0; true; i++) {
 //        string * request;
